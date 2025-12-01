fe45c0a5c266c7eb99a652088ca8162b757ebe63
Beau.s <9isa.beau@gmail.com>
2025-12-01 10:31:10 +0100

    web routes fix

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// student 3
-Route::prefix('vak')->group(function () {
 
-   
-    Route::get('/', [VakController::class, 'index'])->name('vak.index');
-    Route::get('/create', [VakController::class, 'create'])->name('vak.create');
-    Route::post('/', [VakController::class, 'store'])->name('vak.store');
-    Route::get('/table', [VakController::class, 'table'])->name('vak.table');
-
-   
-    Route::get('/{vak}', [VakController::class, 'show'])->name('vak.show');
-    Route::get('/{vak}/edit', [VakController::class, 'edit'])->name('vak.edit');
-    Route::put('/{vak}', [VakController::class, 'update'])->name('vak.update');
-    Route::delete('/{vak}', [VakController::class, 'destroy'])->name('vak.destroy');
-});
 
 
 require __DIR__.'/auth.php';
+require __DIR__.'/beauweb.php';
